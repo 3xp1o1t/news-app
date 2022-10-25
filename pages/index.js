f8c0458dbab8d37a85e556ccd6f9952ab056fe40
3xp1o1t <jes.monti.503@gmail.com>
2022-10-25 13:56:56 -0500

    Desde ahora se puede comsumir el api de news
    Se agrego un metodo para consumir el api News.
    Se agregaron los links de header.
    Se agrego una configuracion para permitir el dominio
    de las noticias de imagenes.

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,58 @@
-import Head from "next/head";
 import Image from "next/image";
 import Link from "next/link";
+import { useState, useEffect } from "react";
 import PageLayout from "../components/PageLayout";
 import styles from "../styles/Home.module.css";
 
-export default function Home() {
+export default function Home({ articles }) {
+
+
   return (
     <PageLayout title="Home">
       <div className={styles.container}>
-        <h1 className={styles.title}>Aprendiendo Next.js desde 0</h1>
-        <Link href="/about">Ir a about</Link>
+        {articles.length === 0 && <p>No tenemos articulos.</p>}
+        {articles.length > 0 &&
+          articles.map((article, index) => (
+            <div key={index}>
+              <Image
+                src={article.urlToImage}
+                alt={`Image for the article ${article.title}`}
+                width={200} height={200}
+                layout='responsive'
+              />
+              <h2>{article.title}</h2>
+              <p>{article.description}</p>
+            </div>
+          ))}
       </div>
     </PageLayout>
   );
+}
+
+// Fetching desde el servidor, recupera los datos cada vez que se renderiza el componente.
+// export async function getServerSideProps() {
+//   const response = await fetch(
+//     "https://newsapi.org/v2/everything?q=tesla&from=2022-09-25&sortBy=publishedAt&apiKey=b1ac9c327907423189801bb4f371a519"
+//   )
+//   const { articles } = await response.json()
+  
+//   return {
+//     props: {
+//       articles
+//     }
+//   }
+// }
+
+// Trae los datos 1 vez al cargar el sitio o cada vez que se recarge la pagina.
+export async function getStaticProps() {
+  const response = await fetch(
+    "https://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey=b1ac9c327907423189801bb4f371a519"
+  );
+  const { articles } = await response.json()
+  
+  return {
+    props: {
+      articles
+    }
+  }
 }
